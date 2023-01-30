9b1a5728d1e0e79ee7035c3e3e77185a68b45250
Stephen Chesnowitz <steve@chesnowitz.com>
2023-01-29 22:36:05 -0500

    printing log in post request in server

--- Desktop/Dev/flask/twt/src/website/auth.py
+++ Desktop/Dev/flask/twt/src/website/auth.py
@@ -1,15 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/log_in')
+@auth.route('/log_in', methods=["GET", "POST"])
 def log_in():
+    data = request.form
+    print(data)
     return render_template('log_in.html', bool=True)
 
 @auth.route('/log_out')
 def log_out():
     return render_template('home.html') 
 
-@auth.route('/sign_up')
+@auth.route('/sign_up', methods=["GET", "POST"])
 def sign_up():
     return render_template('sign_up.html')    
