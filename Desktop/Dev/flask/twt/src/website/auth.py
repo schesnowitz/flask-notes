56eaee8e81c5f5aa79cbf4779339b0f3b2d18c7b
Stephen Chesnowitz <steve@chesnowitz.com>
2023-01-29 22:54:15 -0500

    stub some sign up checks

--- Desktop/Dev/flask/twt/src/website/auth.py
+++ Desktop/Dev/flask/twt/src/website/auth.py
@@ -4,8 +4,7 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/log_in', methods=["GET", "POST"])
 def log_in():
-    data = request.form
-    print(data)
+
     return render_template('log_in.html', bool=True)
 
 @auth.route('/log_out')
@@ -14,4 +13,22 @@ def log_out():
 
 @auth.route('/sign_up', methods=["GET", "POST"])
 def sign_up():
+    if request.method == "POST":
+        email = request.form.get('email')
+        first_name = request.form.get('firstname')
+        first_name = request.form.get('firstname')
+        password_1 = request.form.get('password1')
+        password_2 = request.form.get('password2')
+
+        if len(email) < 4:
+            pass
+        elif len(first_name) < 2:
+            pass
+        elif password_1 != password_2:
+            pass
+        elif password_1 < 7:
+            pass
+        else:
+            #  add user to database
+            pass
     return render_template('sign_up.html')    
